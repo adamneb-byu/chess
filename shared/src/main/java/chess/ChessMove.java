f49d2c408164c581b7c6b85b8decf97bcbeecf13
thirteenling <adamnebeker1@gmail.com>
2026-09-04 22:13:04 -0600

    Added generic override methods equals and hashCode for future implementation to ChessBoard, ChessMove, ChessPiece, and ChessPosition.

--- shared/src/main/java/chess/ChessMove.java
+++ shared/src/main/java/chess/ChessMove.java
@@ -35,4 +35,15 @@ public class ChessMove {
     public ChessPiece.PieceType getPromotionPiece() {
         throw new RuntimeException("Not implemented");
     }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
 }
