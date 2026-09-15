6a417e8d48f5573e50a8ee37aad2e452e0f9fc6d
adamneb-byu <adamneb@byu.edu>
2026-09-15 17:31:27 -0600

    Passes tests for stalemate

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -1,5 +1,6 @@
 package chess;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Objects;
@@ -183,7 +184,31 @@ public class ChessGame {
      * @return True if the specified team is in stalemate, otherwise false
      */
     public boolean isInStalemate(TeamColor teamColor) {
-        throw new RuntimeException("Not implemented");
+        boolean stalemate = true;
+        // Simulate every single possible move, set to false if king can move
+        for(int i = 1; i <= 8; i++){
+            for(int j = 1; j <= 8; j++){
+                ChessPosition curPos = new ChessPosition(i,j);
+                if(board.getPiece(curPos) != null &&
+                        board.getPiece(curPos).getTeamColor() == teamColor){
+                    ArrayList<ChessMove> moves = (ArrayList<ChessMove>) validMoves(curPos);
+                    if (!moves.isEmpty() || isInCheck(teamColor)){
+                        stalemate = false;
+                    }
+                    /*for(ChessMove move : moves){
+                        ChessBoard simBoard = simulateMove(move);
+                        ArrayList<ChessMove> kingMoves = (ArrayList<ChessMove>) PieceMovesCalculator.pieceMoves(simBoard,findKing(teamColor,simBoard));
+                        ArrayList<ChessMove> attacks = (ArrayList<ChessMove>) AttackManager.getTeamMoves(getOtherTeam(teamColor),true,simBoard);
+                        for(ChessMove kingMove : kingMoves) {
+                            if (!AttackManager.isUnderAttack(kingMove.getEndPosition(), attacks)) {
+                                stalemate = false;
+                            }
+                        }
+                    }*/
+                }
+            }
+        }
+        return stalemate;
     }
 
     /**
